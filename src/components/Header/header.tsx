95e1af7de73deebadd3f942263053299a02bc024
Neel123priyansh <neelpriyansh@gmail.com>
2025-09-16 23:59:37 +0530

    Again OTP Fix

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -8,7 +8,7 @@ export default function header() {
         <ul className='flex flex-row text-2xl text-[#00df9a] gap-3 mr-[30px] font-urbanist font-semibold '>
           <li className='p-4 hover:border-b-2 hover:border-[#00df9a] hover:border-opacity-60'>Home</li>
           <li className='p-4 hover:border-b-2 hover:border-[#00df9a] hover:border-opacity-60 '>About Us</li>
-          <li className='p-4 hover:border-b-2 hover:border-[#00df9a] hover:border-opacity-60'>Price</li >
+          <li className='p-4 hover:border-b-2 hover:border-[#00df9a] hover:border-opacity-60'>Price</li > 
           <li className='p-4 hover:border-b-2 hover:border-[#00df9a] hover:border-opacity-60'>Contact Us</li>
         </ul>
     </div> 
